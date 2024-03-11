3127467d6efc1eec797793136a782380e356cb29
jiannotti66 <121405260+jiannotti66@users.noreply.github.com>
2024-03-11 00:16:49 -0500

    commit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const NavBar = () => {
   return (
     <div className='text-red-500 text-center text-3xl font-bold font-serif'>
-        Jacks Portfolio
+        Jacks Portfolio!
     </div>
   );
 }
