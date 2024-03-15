e692641de10cc111fb449dd29d4eff1ed85f19ef
jiannotti66 <121405260+jiannotti66@users.noreply.github.com>
2024-03-15 13:06:54 -0500

    made button able to scroll when clicked, make it scroll to work component when made.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Typed } from 'react-typed'; // Correct import
 
 const Hero = () => {
+  const aboutRef = useRef(null);
+
+  const handleClick = () => {
+    if (aboutRef.current) {
+      aboutRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <div className='text-white'>
       <div className='max-w-[800px] mt-[100px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
         <p className='text-[#00df9a] font-bold p-2 md:text-5xl'>
-          Hello! Im Jack
+          Hello! I'm Jack
         </p>
         <h1 className='md:text-4xl mt-6 mt- text-xl font-bold text-gray-500'>
-          Im a college student from chicago
+          I'm a college student from Chicago
         </h1> 
-        <div class="w-fit">
+        <div className="w-fit">
           {/* try to make this wider for the text if possible */}
-          <p class='text-center md:text-4xl sm:text-4xl text-6x2 font-bold md:py-8'>I am 18 years old with 4 years of computer science experience. Currently, I'm focused on exploring within the cybersecurity field. I am open to any work, research, or internship opportunities within my field.</p>
+          <p className='text-center md:text-4xl sm:text-4xl text-6x2 font-bold md:py-8'>I am 18 years old with 4 years of computer science experience. Currently, I'm focused on exploring within the cybersecurity field. I am open to any work, research, or internship opportunities within my field.</p>
         </div>
-        <button style={{cursor:'pointer'}} smooth={true} duration={500} className='w-3/5 mx-auto text-white group border-2 px-6 py-3 my-2 flex items-center justify-center hover:bg-[#00df9a] hover:border-[#4a4a4a] hover: duration-300'>View Work</button>
-
+        <button 
+          style={{ cursor: 'pointer' }} 
+          onClick={handleClick} 
+          className='w-3/5 mx-auto text-white group border-2 px-6 py-3 my-2 flex items-center justify-center hover:bg-[#00df9a] hover:border-[#4a4a4a] hover: duration-300'
+        >
+          View Work
+        </button>
       </div>
+      <div ref={aboutRef}></div>
     </div>
-  
-
   );
 };
 
